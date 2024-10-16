8f57ec3915f6061d40448ddb6c1c5f8da9d6a8ca
cmon-devops <rishmarun18@gmail.com>
2024-10-16 14:41:21 +0530

    Update create_ec2.sh

--- aws-cli-setup/create_ec2.sh
+++ aws-cli-setup/create_ec2.sh
@@ -1,1 +1,37 @@
+#!/bin/bash
+# Script to create an EC2 instance using AWS CLI
+
+AMI_ID="ami-0abcdef1234567890"  # Change this to your preferred AMI ID (Ubuntu 20.04)
+INSTANCE_TYPE="t2.micro"
+KEY_NAME="my-ec2-keypair"  # Ensure you have a key pair created
+SECURITY_GROUP="my-sg"
+
+# Create a Security Group that allows SSH and MongoDB access (port 27017)
+aws ec2 create-security-group --group-name $SECURITY_GROUP --description "Security group for MongoDB"
+
+# Add rules to Security Group
+aws ec2 authorize-security-group-ingress --group-name $SECURITY_GROUP --protocol tcp --port 22 --cidr 0.0.0.0/0
+aws ec2 authorize-security-group-ingress --group-name $SECURITY_GROUP --protocol tcp --port 27017 --cidr 0.0.0.0/0
+
+# Launch EC2 instance
+INSTANCE_ID=$(aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --count 1 \
+    --instance-type $INSTANCE_TYPE \
+    --key-name $KEY_NAME \
+    --security-groups $SECURITY_GROUP \
+    --query 'Instances[0].InstanceId' \
+    --output text)
+
+echo "Instance created with ID: $INSTANCE_ID"
+
+# Wait until instance is running
+aws ec2 wait instance-running --instance-ids $INSTANCE_ID
+
+# Get public DNS of the instance
+PUBLIC_DNS=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicDnsName' --output text)
+
+echo "Instance Public DNS: $PUBLIC_DNS"
+
+echo "EC2 instance created. Now run the 'mongodb_ec2_setup.sh' script to configure MongoDB on the instance."
 
