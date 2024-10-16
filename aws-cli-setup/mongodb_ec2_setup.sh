4e769df296d1b0a2ae59b60ce892467fcc91f6e6
cmon-devops <rishmarun18@gmail.com>
2024-10-16 14:43:26 +0530

    Update mongodb_ec2_setup.sh

--- aws-cli-setup/mongodb_ec2_setup.sh
+++ aws-cli-setup/mongodb_ec2_setup.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
+# Script to SSH into EC2 and install MongoDB
+
+PUBLIC_DNS=$(cat ec2_dns.txt)
+KEY_PATH="~/.ssh/my-ec2-keypair.pem"  # Path to your key pair
+
+# SSH into EC2 and install MongoDB
+ssh -o "StrictHostKeyChecking=no" -i $KEY_PATH ubuntu@$PUBLIC_DNS << 'EOF'
+    sudo apt update
+    sudo apt install -y mongodb
+
+    # Start and enable MongoDB service
+    sudo systemctl start mongodb
+    sudo systemctl enable mongodb
+
+    echo "MongoDB installed and running on EC2 instance."
+EOF
 
